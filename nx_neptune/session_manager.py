0dbcf0a3616948b535d525d4e2bdfdd0bf87cf71
Andrew Carbonetto <andrew.carbonetto@improving.com>
2025-12-12 16:42:11 -0800

    Clean up notebooks
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- nx_neptune/session_manager.py
+++ nx_neptune/session_manager.py
@@ -139,7 +139,7 @@ class SessionManager:
         Returns:
             bool: True if permissions are valid, False otherwise.
         """
-        return self._neptune_client
+        return instance_management.validate_permissions()
 
     def list_graphs(self, with_details=False):
         """List available Neptune Analytics graphs.
