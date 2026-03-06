ab5215fa0736666bf67c54018c198f82e40c487f
Andy Kwok <andy.kwok@improving.com>
2026-03-05 18:50:48 -0800

    Update notebook

--- nx_neptune/utils/utils.py
+++ nx_neptune/utils/utils.py
@@ -314,7 +314,7 @@ def generate_create_table_ddl(table_name, s3_location, columns):
 
 def generate_projection_stmt(
     col_id, base_table, columns=None, col_label=None, col_embedding=None, joins=None,
-    udf_lookup=None, vector_bucket=None, vector_index=None
+    connector_name=None, vector_bucket=None, vector_index=None, col_vector_id=None
 ):
     """
     Generate a SQL SELECT statement for projecting data with Neptune-compatible column names.
@@ -381,14 +381,14 @@ def generate_projection_stmt(
             selects.append(f'{col} AS "{col_name}"')
 
     if col_embedding:
-        if udf_lookup:
-            selects.append(
-                f"array_join(transform(get_embedding('{vector_bucket}', '{vector_index}', {col_embedding}), x -> cast(x AS varchar)), ';') AS \"embedding:vector\""
-            )
-        else:
-            selects.append(
-                f"array_join(transform({col_embedding}, x -> cast(x AS varchar)), ';') AS \"embedding:vector\""
-            )
+        selects.append(
+            f"array_join(transform({col_embedding}, x -> cast(x AS varchar)), ';') AS \"embedding:vector\""
+        )
+
+    if col_vector_id:
+        selects.append(
+            f"array_join(transform(get_embedding('{vector_bucket}', '{vector_index}', {col_vector_id}), x -> cast(x AS varchar)), ';') AS \"embedding:vector\""
+        )
 
     select_clause = ",\n        ".join(selects)
 
@@ -397,16 +397,16 @@ def generate_projection_stmt(
         for table, condition in joins:
             from_clause += f" join\n    {table} \n    on {condition}"
 
-    if udf_lookup:
+    if connector_name:
         return f"""
-                USING 
-                EXTERNAL FUNCTION get_embedding(schema_name VARCHAR, index_name VARCHAR, id VARCHAR ) 
-                    RETURNS ARRAY<REAL>
-                LAMBDA '{udf_lookup}'
-                SELECT
-                    {select_clause},
-                    row_number() OVER () AS bucket
-                FROM {from_clause};"""
+            USING 
+            EXTERNAL FUNCTION get_embedding(schema_name VARCHAR, index_name VARCHAR, id VARCHAR ) 
+                RETURNS ARRAY<REAL>
+            LAMBDA '{connector_name}'
+            SELECT
+                {select_clause},
+                row_number() OVER () AS bucket
+            FROM {from_clause};"""
     else:
         return f"""
           SELECT
