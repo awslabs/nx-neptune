1f4f14fcee7aa6486e23cd59df4e8b59f881a185
Andy Kwok <kjhiufun@amazon.com>
2026-07-10 12:57:29 -0700

    Update code review

--- proxy/src/nx_neptune_proxy/app.py
+++ proxy/src/nx_neptune_proxy/app.py
@@ -45,8 +45,8 @@ app = FastAPI(title="nx-neptune-proxy", version="0.1.0", docs_url="/docs", redoc
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.allowed_origins,
-    allow_methods=["GET", "POST", "DELETE"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 
