1d09f8eb76dbee670a7a7b8bbfa1e9db37cbc71a
Andy Kwok <kjhiufun@amazon.com>
2026-07-15 17:32:28 -0700

    Swap wording from sesions to projection

--- proxy/ui-src/src/App.tsx
+++ proxy/ui-src/src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useMemo } from "react";
 import { Routes, Route, Navigate, useNavigate } from "react-router";
 import { Sidebar } from "./components/Sidebar";
 import { Import } from "./pages/Import";
-import { Sessions } from "./pages/Sessions";
+import { Projections } from "./pages/Projections";
 import { Graphs } from "./pages/Graphs";
 import { Projects } from "./pages/Projects";
 import { Landing } from "./pages/Landing";
@@ -26,7 +26,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/import" element={<Import />} />
-          <Route path="/sessions" element={<Sessions />} />
+          <Route path="/projections" element={<Projections />} />
           <Route path="/graphs" element={<Graphs />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
