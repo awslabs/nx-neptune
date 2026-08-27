6c6ff3f85f9ff96778e048edff4d9b0937ea2dfa
Andy Kwok <kjhiufun@amazon.com>
2026-08-27 14:38:25 -0700

    Authro update

--- docs-site/astro.config.mjs
+++ docs-site/astro.config.mjs
@@ -22,6 +22,7 @@ export default defineConfig({
             },
             andrewCarbonetto: { name: 'Andrew Carbonetto', url: 'https://github.com/acarbonetto' },
             andyKwok: { name: 'Andy Kwok (Hiu Fung)', url: 'https://github.com/andy-k-improving' },
+            ozanEken: { name: 'Ozan Eken' },
           },
         }),
       ],
